778221a74b04730769154a0a7f7b9b6fb4c83f34
shashankrana5 <shashankrana316@gmail.com>
2022-11-25 10:52:36 -0500

    added login and authentication context.

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,24 @@
+import { useAuthContext } from "../hooks/UseAuthContext"
+
+
+export const useLogin = () => {
+
+    const { dispatch } = useAuthContext();
+
+    const login = async(email, password) => {
+
+        const response = await fetch("/api/users/login", {
+            
+            method: "POST", 
+            headers: {"Content-Type": "application/json"}, 
+            body: JSON.stringify({email,password})
+        });
+        const json = await response.json()
+        if (response.ok){
+            localStorage.setItem("user", JSON.stringify(json))
+
+            dispatch({type: "LOGIN", payload: json})
+        }
+    }
+    return {login}
+}
